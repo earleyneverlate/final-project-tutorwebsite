c7c48193396140bca6b77337876408e7d1bddc87
Shameemah <Shameemah@shameemah.local>
2018-11-28 10:55:54 -0600

    Delete directories and change directory layout

--- server.js
+++ server.js
@@ -4,11 +4,24 @@
 let express = require('express'),
     app = express(),
     port = process.env.PORT || 3000,
+    appointmentRoutes = require("./appointment-routes"),
+    bodyParser = require('body-parser'),
     mongoose = require('mongoose');
+
+//Allow body parser to handle post requests
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.set('view engine', 'pug')
+app.set('views', __dirname + '/views');
 
 //Database configuration
 mongoose.connect('mongodb://localhost/tutorwebsite');
 var db = mongoose.connection;
+
+//Using appointmentRoutes which defines all the API endpoints
+app.use('/', appointmentRoutes)
 
 app.listen(port);
 console.log('Server is running on port: ' + port);
