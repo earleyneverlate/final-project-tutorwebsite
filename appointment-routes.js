ca0582b9f418d195200fb55bf003c2b079fa535e
Shameemah <Shameemah@shameemah.local>
2018-11-28 10:55:06 -0600

    Add route for displaying appointment form

---
+++ appointment-routes.js
@@ -0,0 +1,35 @@
+//File Name: appointment-routes.js - API endpoints for scheduling scheduling
+//appointments are defined here
+
+//Imports
+let express = require('express'),
+    app = express();
+let router = require('express').Router();
+var appointmentController = require('./appointment-controller');
+
+//Default API response
+router.get('/', function (req, res) {
+    res.json({
+       status: 'Schedule Appointment API Working!',
+       message: 'Welcome!',
+    });
+});
+
+//apppointment routes
+router.route('/books/new')
+    .get(appointmentController.newappointment)
+    .post(appointmentController.addappointment)
+
+//appointment API routes
+router.route('/api/appointment')
+    .get(appointmentController.index)
+    .post(appointmentController.new);
+
+router.route('/api/appointment/:appointment_id')
+    .get(appointmentController.view)
+    .put(appointmentController.update)
+    .delete(appointmentController.delete);
+
+
+//Export module
+module.exports = router;
