8b05bd0a7df166530c0d105240dece32ec0b8865
Shameemah <Shameemah@dhcp199.merunorth103.iit.edu>
2018-12-02 19:12:41 -0600

    Included route for main tutor view page

--- routes.js
+++ routes.js
@@ -23,6 +23,9 @@ router.route('/tutor/new')
     .get(tutorviewController.newtutor)
     .post(tutorviewController.addtutor)
 
+router.route('/tutor/view')
+    .get(tutorviewController.alltutors)
+
 //student routes
 router.route('/student/new')
     .get(studentviewController.newstudent)
