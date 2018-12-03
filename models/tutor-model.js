b0de317f88b2c32ab700641c61717cad6d05e68d
Shameemah <Shameemah@dhcp199.merunorth103.iit.edu>
2018-12-02 20:03:16 -0600

    Add location field to tutor model

--- models/tutor-model.js
+++ models/tutor-model.js
@@ -6,6 +6,7 @@ let mongoose = require('mongoose');
 //Database Schema
 var tutorViewSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  location: { type: String, required: true },
   grade: { type: String, required: true },
   subject: { type: String, required: true },
   availability: { type: String, required: true }
