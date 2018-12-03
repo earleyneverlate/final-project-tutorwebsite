a3f782af1de3c416b2b92a6c3ca2fc85a25bec11
Dalilah Earley <dearley@hawk.iit.edu>
2018-12-02 21:04:46 -0600

    update register-model.js

--- models/register-model.js
+++ models/register-model.js
@@ -5,6 +5,7 @@ let mongoose = require('mongoose');
 
 //Database Schema
 var registerSchema = new mongoose.Schema({
+  username: { type: String, required: true },
   first: { type: String, required: true },
   last: { type: String, required: true },
   email: { type: String, required: true }, 
